5e940ed0bbe07ae2cfe57e457044ede6abcad1da
douglas <douglas.sena@mv.com.br>
2019-10-23 16:48:27 -0300

    Formatar data.

--- src/app/list-cars/list-cars.component.ts
+++ src/app/list-cars/list-cars.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AutoMobileService, AutoParams } from '../services/auto-mobile.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AutoMobileService } from '../services/auto-mobile.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CheaperRent } from '../models';
-import { debounceTime } from 'rxjs/operators';
-import { filter } from 'minimatch';
 
 @Component({
   selector: 'app-list-cars',
@@ -20,42 +18,24 @@ export class ListCarsComponent implements OnInit {
     private _autoMobileService: AutoMobileService,
   ) {
     this.carsForm = fb.group({
-      startDate: new Date(),
-      endDate: new Date(),
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      email: ['', Validators.required],
       loyaltyProgram: false
     });
   }
 
   ngOnInit() {
     this.getCar();
-
-    this.carsForm.valueChanges
-      .pipe(debounceTime(300))
-      .subscribe(filters => {
-        filters.startDate = filters.startDate ? this._formatDate(filters.startDate) : null;
-        filters.endDate = filters.endDate ? this._formatDate(filters.endDate) : null;
-        this.getCar(filters);
-      });
   }
 
-  getCar(params?: AutoParams): void {
-    this._autoMobileService.getCheaperRent(params)
+  getCar(): void {
+    this._autoMobileService.getCheaperRent(this.carsForm.value)
       .subscribe(cheaperRent => this.cheaperRent = cheaperRent);
   }
 
-  private _formatDate(date: Date): string {
-    const d = new Date(date);
-    let month = '' + (d.getMonth() + 1);
-    let day = '' + d.getDate();
-    const year = d.getFullYear();
-
-    if (month.length < 2) {
-      month = '0' + month;
-    }
-    if (day.length < 2) {
-      day = '0' + day;
-    }
-
-    return [year, month, day].join('-');
+  canSearch(): boolean {
+    return this.carsForm && this.carsForm.invalid;
   }
 }
+
--- src/app/services/auto-mobile.service.ts
+++ src/app/services/auto-mobile.service.ts
@@ -7,6 +7,7 @@ import { Manufacturer, Categories, Model, ModelYear, CheaperRent } from '../mode
 export interface AutoParams {
     startDate?: string;
     endDate?: string;
+    email?: string;
     loyaltyProgram?: boolean;
 }
 
@@ -16,6 +17,8 @@ export class AutoMobileService {
     constructor(private _http: HttpClient) { }
 
     getCheaperRent(params?: AutoParams): Observable<CheaperRent> {
+        params.startDate = params.startDate ? this._formatDate(new Date(params.startDate)) : null;
+        params.endDate = params.endDate ? this._formatDate(new Date(params.endDate)) : null;
 
         const url = `${herokuUrl.url}/car-rental/cheaper-rent`;
 
@@ -24,6 +27,7 @@ export class AutoMobileService {
                 params: new HttpParams()
                     .set('startDate', params.startDate ? params.startDate : null)
                     .set('endDate', params.endDate ? params.endDate : null)
+                    .set('email', params.email ? params.email : null)
                     .set('loyaltyProgram', params.loyaltyProgram.toString())
             };
             return this._http.get<CheaperRent>(url, optionsHttp);
@@ -60,5 +64,22 @@ export class AutoMobileService {
     listYearsByManufactureAndModelId(manufactureId: number, modelId: number): Observable<ModelYear[]> {
         const url = `${herokuUrl.url}/car-rental/manufacturers/${manufactureId}/models/${modelId}`;
         return this._http.get<ModelYear[]>(url);
+    }
+
+
+    private _formatDate(date: Date): string {
+        const d = new Date(date);
+        let month = '' + (d.getMonth() + 1);
+        let day = '' + d.getDate();
+        const year = d.getFullYear();
+
+        if (month.length < 2) {
+            month = '0' + month;
+        }
+        if (day.length < 2) {
+            day = '0' + day;
+        }
+
+        return [year, month, day].join('-');
     }
 }
